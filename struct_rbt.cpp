2d5601009b127c4477f1be635674bf54d8e7e4d5
RECOSTRO <vladislavakhmetov09@gmail.com>
2026-09-18 11:41:40 +0300

    Create func(delete) and template class

--- struct_rbt.cpp
+++ struct_rbt.cpp
@@ -1,7 +1,49 @@
-#include <iostream>
 #include <struct_rbt.hpp>
 using namespace std;
 
+
+// функция поиска
+template <typename mytypel>
+rbt<mytypel> *search(rbt<mytypel> *head, int key){
+    if (head == nullptr) return nullptr;
+
+    rbt<mytypel> *temp = head;
+
+    while (temp != nullptr){
+        if (key < temp->key) temp = temp->left;
+        else if (key > temp->key) temp = temp->right;
+        else break;
+    }
+
+    return temp;
+}
+////////////////////
+
+// функция поиска максимуму
+template <typename mytypel>
+rbt<mytypel> *search_max(rbt<mytypel> *head){
+    if (head == nullptr) return nullptr;
+
+    rbt<mytypel> *temp = head;
+
+    while (temp->right != nullptr) temp = temp->right;
+
+    return temp;
+}
+////////////////////
+
+// функция поиска минимума
+template <typename mytypel>
+rbt<mytypel> *search_min(rbt<mytypel> *head){
+    if (head == nullptr) return nullptr;
+
+    rbt<mytypel> *temp = head;
+
+    while (temp->left != nullptr) temp = temp->left;
+
+    return temp;
+}
+////////////////////
 
 // функция инициализации дерева
 template <typename mytypel>
@@ -19,8 +61,11 @@ void make_tree(mytypel data, rbt<mytypel> *( &head), int key){
 }
 /////////////////////
 
+// функция вставки узла в кчд
 template <typename mytypel>
 void insert(rbt<mytypel> *( &head), int key, mytypel data){
+    if (head == nullptr) return;
+
     rbt<mytypel> *temp = head;
     rbt<mytypel> *point = temp;
 
@@ -55,10 +100,11 @@ void insert(rbt<mytypel> *( &head), int key, mytypel data){
         point->right = poison;
     }
 
-    balance_rbt(poison);// добавим на следующим релизе
+    balance_insert(poison);// добавим на следующим релизе
 }
+/////////////////////
 
-// ещё одна функция инициализации дерева по совету коментатора
+// ещё одна функция инициализации дерева по совету коментатора (перегрузка функции)
 template <typename mytypel>
 void  make_tree(Node<mytypel> *storage, rbt<mytypel> *( &head), int *key, int len){
     if (head == nullptr){
@@ -77,6 +123,48 @@ void  make_tree(Node<mytypel> *storage, rbt<mytypel> *( &head), int *key, int le
 }
 /////////////////////
 
+// функция удаление узла из дерева
+template <typename mytypel>
+void delete_node(rbt<mytypel> *(&head), int key){
+    if (head == nullptr) return;
+
+    rbt<mytypel> *temp = head;
+
+    while(temp != nullptr){
+        if (temp->key == key) break;
+        else if (temp->key > key) temp = temp->left;
+        else temp = temp->right;
+    }
+
+    if (temp == nullptr) return;
+
+    if (temp == head){// условие на один узел в дереве
+        delete head;
+        return;
+    }
+
+    rbt<mytypel> *point = temp->parent;
+
+    if (point->left == temp) {
+
+        if (temp->right == nullptr) point->left = temp->left;
+        else if (temp->left == nullptr) point->left = temp->right;
+        else point->left = search_min(temp->right);
+
+        delete temp;
+
+        balance_del(point->left); // реализованно будет чуть позже
+    } else if (point->right) {
+
+        if (temp->right == nullptr) point->right = temp->left;
+        else if (temp->left == nullptr) point->right = temp->right;
+        else point->right = search_min(temp->right);
+
+        delete temp;
+
+        balance_del(point->right); // реализованно будет чуть позже
+    }
+}
 
 // функция очистки дерева
 template <typename mytypel>
@@ -91,47 +179,3 @@ void freel(rbt<mytypel> *( &head)){
     head = nullptr;
 }
 /////////////////////
-
-// функция поиска
-template <typename mytypel>
-rbt<mytypel> *search(rbt<mytypel> *head, int key){
-    if (head == nullptr) return nullptr;
-
-    rbt<mytypel> *temp = head;
-
-    while (temp != nullptr){
-        if (key < temp->key) temp = temp->left;
-        else if (key > temp->key) temp = temp->right;
-        else break;
-    }
-
-
-    return temp;
-}
-////////////////////
-
-// функция поиска максимуму
-template <typename mytypel>
-rbt<mytypel> *search_max(rbt<mytypel> *head){
-    if (head == nullptr) return nullptr;
-
-    rbt<mytypel> *temp = head;
-
-    while (temp->right != nullptr) temp = temp->right;
-
-    return temp;
-}
-////////////////////
-
-// функция поиска минимума
-template <typename mytypel>
-rbt<mytypel> *search_min(rbt<mytypel> *head){
-    if (head == nullptr) return nullptr;
-
-    rbt<mytypel> *temp = head;
-
-    while (temp->left != nullptr) temp = temp->left;
-
-    return temp;
-}
-////////////////////
